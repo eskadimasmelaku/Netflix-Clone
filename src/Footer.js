65b2f6f2ec54b5309322a38fe51e9f91afd4b3ca
Eskadimas melaku <eskadimasmelaku@gmail.com>
2022-08-14 20:14:21 +0300

    my first project netflix project

---
+++ src/Footer.js
@@ -0,0 +1,82 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+	return (
+		<div className="footer">
+			<div className="footer__question">
+				<p>Questions? Contact us.</p>
+			</div>
+			<div className="footer__links">
+				<ul>
+					<li>
+						<a href="#">FAQ</a>{" "}
+					</li>
+					<li>
+						<a href="#">Media Center</a>
+					</li>
+					<li>
+						<a href="#">Ways to Watch</a>
+					</li>
+					<li>
+						<a href="#">Cookie Preferences</a>
+					</li>
+					<li>
+						<a href="#">Speed Test</a>
+					</li>
+				</ul>
+				<ul>
+					<li>
+						<a href="#">Help Center</a>
+					</li>
+					<li>
+						<a href="#">Investor Relations</a>
+					</li>
+					<li>
+						<a href="#">Terms of Use</a>
+					</li>
+					<li>
+						<a href="#">Corporate information</a>
+					</li>
+					<li>
+						<a href="#">Legal Notuces</a>
+					</li>
+				</ul>
+
+				<ul>
+					<li>
+						<a href="#">Account</a>
+					</li>
+					<li>
+						<a href="#">Jobs</a>
+					</li>
+					<li>
+						<a href="#">Privacy</a>
+					</li>
+					<li>
+						<a href="#">Contact Us</a>
+					</li>
+					<li>
+						<a href="#">Only on Netifilix</a>
+					</li>
+				</ul>
+				<ul>
+					<li>
+						<a href="#">Media Center</a>
+					</li>
+					<li>
+						<a href="#">Terms of Use</a>
+					</li>
+					<li>
+						<a href="#">Contact Us</a>
+					</li>
+				</ul>
+			</div>
+			<h1>
+				Built By: <a href="https://www.eyuas.com/">Eskadimas Melaku</a>
+			</h1>
+		</div>
+	);
+}
+
+export default Footer;
